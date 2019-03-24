2410830e663f02fe19deabb31ead2e7f93fac2c9
yorkehan <740081506@qq.com>
2019-03-25 00:50:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <div style="width: 80%;height:80%;margin:100px auto;">
+    <el-row :gutter="20">
+      <el-col v-for="item in docs" style="margin-top: 20px" :span="6" >
+        <el-card  shadow="hover" @click.native="godoc(item.url)" style="height: 200px;cursor: pointer" >
+          {{item.name}}
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      ip: 'http://192.168.1.2:8080/',
+      docs: [
+        {
+          url: 'vue',
+          name: 'Vue.js'
+        },
+        {
+          url: 'element-ui',
+          name: 'element-ui'
+        },
+        {
+          url: 'element-ui-admin',
+          name: 'element-ui-admin'
+        },
+        {
+          url: 'mybatis-plus',
+          name: 'mybatis-plus'
+        }
+      ]
+    }
+  },
+  methods: {
+    godoc: function (url) {
+      window.open(this.ip + url, '_blank')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
